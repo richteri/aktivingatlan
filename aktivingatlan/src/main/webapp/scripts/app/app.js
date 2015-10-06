b7c367520883f71e34f5adcf4eb4a2f765b9c2f1
richteri <gmcreator@gmail.com>
2015-10-06 20:20:33 +0200

    Included angular-ui-grid

--- aktivingatlan/src/main/webapp/scripts/app/app.js
+++ aktivingatlan/src/main/webapp/scripts/app/app.js
@@ -3,6 +3,7 @@
 angular.module('aktivingatlanApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalprecht.translate', 
                'ui.bootstrap', // for modal dialogs
                'ui.select', // for tag-like multi-select
+               'ui.grid',
     'ngResource', 'ui.router', 'ngCookies', 'ngCacheBuster', 'ngFileUpload', 'infinite-scroll', 'ngSanitize'])
 
     .run(function ($rootScope, $location, $window, $http, $state, $translate, Language, Auth, Principal, ENV, VERSION) {
