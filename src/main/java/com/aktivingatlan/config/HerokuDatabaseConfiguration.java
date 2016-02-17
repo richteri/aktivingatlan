908e6d183b346501202137d29dab7530b9ad7bb7
richteri <istvan.richter@adwareresearch.com>
2016-02-17 16:10:37 +0100

    Updated HerokuDatabaseConfiguration class

--- src/main/java/com/aktivingatlan/config/HerokuDatabaseConfiguration.java
+++ src/main/java/com/aktivingatlan/config/HerokuDatabaseConfiguration.java
@@ -7,6 +7,8 @@ import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.context.ApplicationContextException;
 import org.springframework.context.EnvironmentAware;
@@ -32,10 +34,11 @@ public class HerokuDatabaseConfiguration implements EnvironmentAware {
     this.propertyResolver = new RelaxedPropertyResolver(environment, "spring.datasource.");
   }
 
-  @Bean
-  public DataSource dataSource() {
+  @Bean(destroyMethod = "close")
+  @ConditionalOnExpression("#{environment.acceptsProfiles('heroku')}")
+  public DataSource dataSource(DataSourceProperties dataSourceProperties, JHipsterProperties jHipsterProperties) {
     log.debug("Configuring Heroku Datasource");
-
+    
     String herokuUrl = propertyResolver.getProperty("heroku-url");
     if (herokuUrl != null) {
       log.info("Using Heroku, parsing their $DATABASE_URL to use it with JDBC");
@@ -55,10 +58,19 @@ public class HerokuDatabaseConfiguration implements EnvironmentAware {
       "?useUnicode=true&characterEncoding=utf8";
 
       HikariConfig config = new HikariConfig();
-      config.setDataSourceClassName(propertyResolver.getProperty("dataSourceClassName"));
+      config.setDataSourceClassName(dataSourceProperties.getDriverClassName());
+      
+      log.debug(dbUrl);
+      
       config.addDataSourceProperty("url", dbUrl);
       config.addDataSourceProperty("user", username);
       config.addDataSourceProperty("password", password);
+      
+      if ("com.mysql.jdbc.jdbc2.optional.MysqlDataSource".equals(dataSourceProperties.getDriverClassName())) {
+          config.addDataSourceProperty("cachePrepStmts", jHipsterProperties.getDatasource().isCachePrepStmts());
+          config.addDataSourceProperty("prepStmtCacheSize", jHipsterProperties.getDatasource().getPrepStmtCacheSize());
+          config.addDataSourceProperty("prepStmtCacheSqlLimit", jHipsterProperties.getDatasource().getPrepStmtCacheSqlLimit());
+      }
       return new HikariDataSource(config);
     } else {
       throw new ApplicationContextException("Heroku database URL is not configured, you must set --spring.datasource.heroku-url=$DATABASE_URL");
